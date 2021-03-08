2b91c40d89c1fc59e713e9ae96773cbcc9c536ed
alisakazimirchik <alice1705@yandex.ru>
2021-03-08 23:35:34 +0100

    Commit 1 lesson Final trial

--- src/main/java/Level3/lesson1/Box.java
+++ src/main/java/Level3/lesson1/Box.java
@@ -24,18 +24,35 @@ public class Box<T extends Fruits> {
         return this.getWeight() == another.getWeight();
     }
 
+    public void sendObjects(Box<T> another) {
+        for (int i = 0; i < arrayList.size(); i++) {
+            another.arrayList.add(arrayList.get(i));
+        }
+        arrayList.clear();
+    }
+
     public static void main(String[] args) {
         Box<Orange> boxOrange = new Box<>(new Orange());
         boxOrange.addToCollection(new Orange());
         boxOrange.addToCollection(new Orange());
-        System.out.println(boxOrange.getWeight());
+        System.out.println("boxOrange has a weight of " + boxOrange.getWeight());
 
         Box<Apple> boxApple = new Box<>(new Apple());
         boxApple.addToCollection(new Apple());
         boxApple.addToCollection(new Apple());
-        System.out.println(boxApple.getWeight());
+        System.out.println("boxApple has a weight of " + boxApple.getWeight());
+        System.out.println("boxApple and boxOrange have same weight: " + boxApple.sameWeight(boxOrange));
 
-        System.out.println(boxApple.sameWeight(boxOrange));
+        Box<Apple> boxApple1 = new Box<>(new Apple());
+        boxApple1.addToCollection(new Apple());
+        boxApple1.addToCollection(new Apple());
+        boxApple1.addToCollection(new Apple());
+        System.out.println("boxApple1 - Before adding new elements: " + boxApple1.getWeight());
+
+        boxApple.sendObjects(boxApple1);
+        System.out.println("boxApple1 - After adding new elements: " + boxApple1.getWeight());
+        System.out.println("boxApple - After sending new elements: " + boxApple.getWeight());
+
     }
 }
 
