a736d3d0963391c71fd9c6656094cd249d739f40
alisakazimirchik <alice1705@yandex.ru>
2021-03-08 23:23:20 +0100

    Commit 1 lesson 2nd trial

--- src/main/java/Level3/lesson1/Box.java
+++ src/main/java/Level3/lesson1/Box.java
@@ -1,4 +1,41 @@
 package Level3.lesson1;
 
-public class Box {
+import java.util.ArrayList;
+
+public class Box<T extends Fruits> {
+
+    private T obj;
+
+    private ArrayList<T> arrayList = new ArrayList<>();
+
+    public Box(T obj) {
+        this.obj = obj;
+    }
+
+    public void addToCollection(T element) {
+        arrayList.add(element);
+    }
+
+    public double getWeight() {
+        return obj.getWeight()* arrayList.size();
+    }
+
+    public boolean sameWeight(Box<?> another) {
+        return this.getWeight() == another.getWeight();
+    }
+
+    public static void main(String[] args) {
+        Box<Orange> boxOrange = new Box<>(new Orange());
+        boxOrange.addToCollection(new Orange());
+        boxOrange.addToCollection(new Orange());
+        System.out.println(boxOrange.getWeight());
+
+        Box<Apple> boxApple = new Box<>(new Apple());
+        boxApple.addToCollection(new Apple());
+        boxApple.addToCollection(new Apple());
+        System.out.println(boxApple.getWeight());
+
+        System.out.println(boxApple.sameWeight(boxOrange));
+    }
 }
+
--- src/main/java/Level3/lesson1/Orange.java
+++ src/main/java/Level3/lesson1/Orange.java
@@ -1,4 +1,10 @@
 package Level3.lesson1;
 
 public class Orange extends Fruits{
+
+    private double weight = 1.5;
+
+    public double getWeight() {
+        return weight;
+    }
 }
